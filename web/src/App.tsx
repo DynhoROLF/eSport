0732ee8e479ec0e6631341464b76418f5e27eb94
Waldyr Romero <dynhoromero@gmail.com>
2022-09-27 21:22:06 -0300

    Aula 1

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,22 @@
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <button>
+      {props.title}
+    </button>
+  )
+}
+
+
 export function App() {
   return (
     <>
+      <Button title="cebola"/>
+      <Button title="roxa"/>
       <h1>Hello World</h1>
     </>
   ) 
