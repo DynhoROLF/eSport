0644a781bc04763f6b602af9dfdce2ebfd91245e
DynhoROLF <dynhoromero@gmail.com>
2022-11-08 15:14:26 -0300

    app mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,24 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello React-Native</Text>
-      <StatusBar style="auto" />
+    <View>
+      
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: '#fff',
-    fontSize: 22
-  }
-});
